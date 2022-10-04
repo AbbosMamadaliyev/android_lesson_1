8d7df815ff493a92affbd7272492a84d46f0615b
Abbos Mamadaliyev <abbbos166@gmail.com>
2022-10-04 23:25:50 +0500

    profile ui

--- app/src/main/java/com/example/android_lesson_1/MainActivity.java
+++ app/src/main/java/com/example/android_lesson_1/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void homeClick(View view) {
 
+        Toast.makeText(this, "Home is clicked", Toast.LENGTH_SHORT).show();
+    }
+    public void contactClick(View view) {
+
+        Toast.makeText(this, "Contact is clicked", Toast.LENGTH_SHORT).show();
+    }
+    public void profileClick(View view) {
+
+        Toast.makeText(this, "Profile page is clicked", Toast.LENGTH_SHORT).show();
     }
 }
